73e89aaf144a02ea3eb59eb5b09a169b7f96b462
rahul shrivastava <rahulshrivastav052@gmail.com>
2025-05-23 00:18:58 +0530

    start

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -7,18 +7,57 @@ import { useState } from 'react';
 
 
 export default function Home() {
-  const [filename, setfilename] = useState(null);
-  const [qid, setqid] = useState(null);
-  const [extractedtext, setextractedtext] = useState(null);
-  const [allchat, setallchat] = useState(['Hello there! How can I help you today?']);
 
   return (
-    <>
-      <div className="w-vh h-svh overflow-clip bg-gray-100 flex flex-col items-center justify-between     ">
-        <Navbar setfilename={setfilename} setqid={setqid} setextractedtext={setextractedtext} filename={filename} />
-        <Conversations allchat={allchat} />
-        <Inputbox qid={qid} extractedtext={extractedtext} setallchat={setallchat} />
+    <div className="flex items-center justify-center relative  h-screen bg-black overflow-clip ">
+
+
+
+      <div className=' flex flex-col items-center justify-center z-10 '>
+        <div className='text-5xl w-full font-mono font-extrabold text-left py-5'>Generate <span className='text-emerald-500'>React</span> Code</div>
+        <form className='  opacity-30 rounded-sm text-white'>
+          <textarea id="message"
+            // value={text}
+            // onChange={handleChange}
+
+            placeholder="  Write your component description here..."
+            rows={10}
+            cols={90}
+            className='p-3 text-white border'  >
+
+          </textarea>
+
+        </form>
+
+
       </div>
-    </>
-  )
-}
+      <div className='absolute top-0 m-auto flex items-center justify-center p-7'>
+        <img src='./logo_text.png' width={'160px'} className=' opacity-100'></img>
+      </div>
+      <div className='bg-lines w-full h-full absolute top-0 left-0 opacity-15 border'></div>
+      <div className='circle-gradient opacity-15 blur-3xl absolute border-white border top-10/12  left-0  h-full w-full'>j</div>
+      {/* <div style={{ perspective: '1000px' }}>
+        <img width={'70px'} className=' logo filter invert-50 hover:invert-0' src={'./logo1.png'}></img>
+      </div> */}
+
+
+
+    </div>
+  );
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+};
+
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -3,7 +3,7 @@
 import React, { useState } from 'react';
 import { FaPlusCircle } from "react-icons/fa";
 import { BsFiletypePdf } from "react-icons/bs";
-import logo from '../../public/logo.png';
+// import logo from '../../public/logo.png';
 
 const Navbar = ({setextractedtext,setfilename,setqid,filename}) => {
 
@@ -15,7 +15,7 @@ const Navbar = ({setextractedtext,setfilename,setqid,filename}) => {
 		<div className=' w-screen max-sm:shadow-lg max-sm:justify-center h-16 max-sm:h-10 bg-white  shadow-sm flex items-center justify-between '>
 				
 			<div className='max-[510px]:scale-75 ml-5 max-[510px]:ml-1 z-10 max-sm:scale-[0.6]'>
-				<img className='transform rotate-12 ' src='/logo.png' width={40} />
+				{/* <img className='transform rotate-12 ' src='/logo.png' width={40} /> */}
 				</div>
 
 
